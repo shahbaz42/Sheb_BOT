da9e4b41f212f39158079988289ea57dd5ce95bc
Shahbaz <shahbaz_ali@outlook.in>
2022-06-08 23:02:08 +0530

    add authorization middleware to routes

--- api/routes/router.js
+++ api/routes/router.js
@@ -1,14 +1,15 @@
 const express = require("express")
 const router = express.Router()
 const { Configuration, OpenAIApi } = require("openai");
+const {authorization} = require("../routes/auth_routes");
 
 let message = "Following is a conversation with Elon Musk, He is the founder, CEO, and Chief Engineer at SpaceX. He is also ceo of Tesla, boring company and Open Ai. <br><br>You: Hey Elon Musk! <br>Elon Musk : Hey Whatsup ?<br>";
 
-router.get("/", (req, res) => {
+router.get("/", authorization, (req, res) => {
     res.render("index", { message });
 });
 
-router.post("/", async (req, res) => {
+router.post("/",  authorization, async (req, res) => {
     try {
         const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY});
         const openai = new OpenAIApi(configuration);
