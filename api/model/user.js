b02e927bb9f46d4d6e2d845fd806a535d97ae870
Shahbaz <shahbaz_ali@outlook.in>
2022-06-08 22:59:44 +0530

    add random name generator

--- api/model/user.js
+++ api/model/user.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const {random_animal_name} = require("../utils/random_name_generator");
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
+        default: random_animal_name
     },
     conversation: [
         {
