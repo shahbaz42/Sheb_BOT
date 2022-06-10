eab687ad35ffcef2337d5272296bfb89fec9f74b
Shahbaz <shahbaz_ali@outlook.in>
2022-06-10 11:42:21 +0530

    code refactoring

--- api/controllers/conversation_controller.js
+++ api/controllers/conversation_controller.js
@@ -1,12 +1,14 @@
 const Conversation = require("../model/conversation");
+const { Configuration, OpenAIApi } = require("openai");
 
 ////////////////testing controller for req.user
 //// remove in production
 exports.get_req_user = (req, res) => {
     res.send(req.user);
 };
+//////////////////////////
 
-exports.index_page_controller = async(req, res) => {
+exports.index_page_controller = async (req, res) => {
     Conversation.find({}, (err, conversations) => {
         if (err) {
             console.log(err);
@@ -17,11 +19,63 @@ exports.index_page_controller = async(req, res) => {
     });
 }
 
+exports.render_chat_page = (req, res) => {
+    const message = req.user.conversation[req.conversation_index].message;
+    const chat_name = req.user.conversation[req.conversation_index].chat_name;
+    const name = req.user.conversation[req.conversation_index].name;
+    res.render("chat", { message, name, chat_name });
+}
+
+exports.handle_reply = async (req, res) => {
+    try {
+        const name = req.user.conversation[req.conversation_index].name;
+        const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+        const openai = new OpenAIApi(configuration);
+
+        let reply = req.body.reply;
+        req.user.conversation[req.conversation_index].message += "<br>You: " + reply + `<br>${name} : `;
+
+        const response = await openai.createCompletion("text-davinci-002", {
+            prompt: req.user.conversation[req.conversation_index].message,
+            temperature: 0.9,
+            max_tokens: 150,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0.6,
+            stop: ["You :", `${name} :`],
+        });
+
+        req.user.conversation[req.conversation_index].message += response.data.choices[0].text + "<br>";
+        await req.user.save();
+        res.json({ message: req.user.conversation[req.conversation_index].message });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error." });
+    }
+}
+
+exports.add_conversation = (req, res) => {
+    const { chat_name, name, message } = req.body;
+
+    const conversation = new Conversation({ chat_name, name, message });
+
+    conversation.save((err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    })
+}
+
+
+// Middlewares
 exports.init_conversation = async (req, res, next) => {
     // This middleware checks if the chat is already present
     // If not, it creates a new chat from the template
     // If yes, it attaches conversation_index to the request
-    
+
     const { chat_name } = req.params;
     const { conversation } = req.user;
 
--- api/routes/router.js
+++ api/routes/router.js
@@ -1,68 +1,37 @@
 const express = require("express")
 const router = express.Router()
-const { Configuration, OpenAIApi } = require("openai");
 const { authorization } = require("../routes/auth_routes");
-const Conversation = require("../model/conversation");
-const {init_conversation} = require("../controllers/conversation_controller");
-const {index_page_controller, get_req_user} = require("../controllers/conversation_controller");
+const { init_conversation } = require("../controllers/conversation_controller");
+const { index_page_controller, get_req_user, render_chat_page, handle_reply, add_conversation } = require("../controllers/conversation_controller");
 
 ///////////////////////
 // testing route for getting req. user
 // remove in production
 
-router.get("/test/req_user", authorization, get_req_user); 
+router.get("/test/req_user",
+    authorization,
+    get_req_user
+);
 
-router.get("/", authorization, index_page_controller );
+router.get("/", 
+    authorization, 
+    index_page_controller
+);
 
-router.get("/chat/:chat_name", authorization, init_conversation, (req, res) => {
-    const message = req.user.conversation[req.conversation_index].message;
-    const chat_name = req.user.conversation[req.conversation_index].chat_name;
-    const name = req.user.conversation[req.conversation_index].name;
-    res.render("chat", { message, name, chat_name });
-});
+router.get("/chat/:chat_name", 
+    authorization, 
+    init_conversation, 
+    render_chat_page
+);
 
-router.post("/chat/:chat_name", authorization, init_conversation, async (req, res) => {
-    try {
-        const name = req.user.conversation[req.conversation_index].name;
-        const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
-        const openai = new OpenAIApi(configuration);
+router.post("/chat/:chat_name", 
+    authorization, 
+    init_conversation, 
+    handle_reply
+);
 
-        let reply = req.body.reply;
-        req.user.conversation[req.conversation_index].message += "<br>You: " + reply + `<br>${name} : `;
-
-        const response = await openai.createCompletion("text-davinci-002", {
-            prompt: req.user.conversation[req.conversation_index].message,
-            temperature: 0.9,
-            max_tokens: 150,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0.6,
-            stop: ["You :", `${name} :`],
-        });
-
-        req.user.conversation[req.conversation_index].message += response.data.choices[0].text + "<br>";
-        await req.user.save();
-        res.json({ message: req.user.conversation[req.conversation_index].message });
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Internal server error." });
-    }
-});
-
-
-router.post("/add_conversation", (req, res) => {
-    const { chat_name, name, message } = req.body;
-
-    const conversation = new Conversation({ chat_name, name, message });
-
-    conversation.save((err, result) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(result);
-        }
-    })
-})
+router.post("/add_conversation", 
+    add_conversation
+);
 
 module.exports = router
